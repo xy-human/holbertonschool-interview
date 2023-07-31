73d297942c68b70eb6c3019e2a2fa3fad8388375
Andrés Castañeda <72631957+xy-human@users.noreply.github.com>
2023-07-31 23:00:07 +0000

    0x1C-island_perimeter

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module contains the island_perimeter method
+"""
+
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid
+    """
+    m, n, Perimeter = len(grid), len(grid[0]), 0
+
+    for i in range(m):
+        for j in range(n):
+            Perimeter += 4*grid[i][j]
+            if i > 0:
+                Perimeter -= grid[i][j]*grid[i-1][j]
+            if i < m-1:
+                Perimeter -= grid[i][j]*grid[i+1][j]
+            if j > 0:
+                Perimeter -= grid[i][j]*grid[i][j-1]
+            if j < n-1:
+                Perimeter -= grid[i][j]*grid[i][j+1]
+
+    return Perimeter
